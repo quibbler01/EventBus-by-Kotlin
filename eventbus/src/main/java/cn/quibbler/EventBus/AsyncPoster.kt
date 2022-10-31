c4b655e329aae97b6a5027d7e9739a12414184ac
quibbler01 <fluent0418@gmail.com>
2022-10-31 22:18:58 +0800

    add AsyncPoster

---
+++ eventbus/src/main/java/cn/quibbler/EventBus/AsyncPoster.kt
@@ -0,0 +1,27 @@
+package cn.quibbler.EventBus
+
+/**
+ * Posts events in background.
+ *
+ * @author Markus
+ */
+class AsyncPoster(private val queue: PendingPostQueue, private val eventBus: EventBus) : Runnable,
+    Poster {
+
+    constructor(eventBus: EventBus) : this(PendingPostQueue(), eventBus)
+
+    override fun run() {
+        val pendingPost: PendingPost? = queue.poll()
+        if (pendingPost == null) {
+            throw IllegalStateException("No pending post available")
+        }
+        eventBus.invokeSubscriber(pendingPost)
+    }
+
+    override fun enqueue(subscription: Subscription, event: Any) {
+        val pendingPost: PendingPost = PendingPost.obtainPendingPost(subscription, event)
+        queue.enqueue(pendingPost)
+        eventBus.getExecutorService().execute(this)
+    }
+
+}
