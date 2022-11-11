19254c1b35991b57769684ac1a9baaa7b2b063ed
quibbler01 <fluent0418@gmail.com>
2022-11-12 00:41:27 +0800

    add PendingPost

---
+++ eventbus/src/main/java/cn/quibbler/EventBus/PendingPost.kt
@@ -0,0 +1,38 @@
+package cn.quibbler.EventBus
+
+class PendingPost(var event: Any?, var subscription: Subscription?) {
+
+    var next: PendingPost? = null
+
+    companion object {
+        private val pendingPostPool: MutableList<PendingPost> = ArrayList()
+
+        fun obtainPendingPost(subscription: Subscription, event: Any?): PendingPost {
+            synchronized(pendingPostPool) {
+                val size = pendingPostPool.size
+                if (size > 0) {
+                    val pendingPost = pendingPostPool.removeAt(size - 1)
+                    pendingPost.event = event
+                    pendingPost.subscription = subscription
+                    pendingPost.next = null
+                    return pendingPost
+                }
+            }
+            return PendingPost(event, subscription)
+        }
+
+        fun releasePendingPost(pendingPost: PendingPost) {
+            pendingPost.event = null
+            pendingPost.subscription = null
+            pendingPost.next = null
+            synchronized(pendingPostPool) {
+                // Don't let the pool grow indefinitely
+                if (pendingPostPool.size < 10000) {
+                    pendingPostPool.add(pendingPost)
+                }
+            }
+        }
+
+    }
+
+}
